5439e096b2fed53300ca8f5f831a095af0bbdeac
Vitaliy Replyuk <vitaliy.replyuk@student.tuke.sk>
2021-12-05 20:16:39 +0100

    add const PATH in database.rb

--- lib/database.rb
+++ lib/database.rb
@@ -1,19 +1,21 @@
 require 'yaml'
 
 class Database
+  PATH = File.expand_path('../..', __FILE__)
+
   def read(file_name)
     if file_name == "/db/searches.yml" &&
-      !File.exist?("#{File.expand_path('../..', __FILE__)}" + "#{file_name}")
+      !File.exist?("#{PATH}" + "#{file_name}")
 
-      File.new("#{File.expand_path('../..', __FILE__)}" + "#{file_name}", 'w')
+      File.new("#{PATH}" + "#{file_name}", 'w')
     end
 
     nil unless File.exist?(file_name)
 
-    YAML.load_file("#{File.expand_path('../..', __FILE__)}" + "#{file_name}")
+    YAML.load_file("#{PATH}" + "#{file_name}")
   end
 
   def write(data, file)
-    File.open("#{File.expand_path('../..', __FILE__)}" + "#{file}", 'w') { |f| f.write(data.to_yaml) }
+    File.open("#{PATH}" + "#{file}", 'w') { |f| f.write(data.to_yaml) }
   end
 end
