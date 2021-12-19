3305679a80cd3bb397062257dd5d99e894a36bac
Vitaliy Replyuk <vitaliy.replyuk@student.tuke.sk>
2021-12-19 22:04:13 +0100

    add Output improvements for statistics

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
+
+gem "i18n", "~> 1.8"
+gem "terminal-table", "~> 3.0"
+gem "colorize", "~> 0.8.1"
--- lib/result_printer.rb
+++ lib/result_printer.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 class ResultPrinter
   def print_result(results)
     puts '-' * 20
@@ -14,9 +15,12 @@ class ResultPrinter
   end
 
   def print_statics (print_total_statistics)
-    puts '-' * 20
-    puts 'Statistic:'
-    puts "Total Quantity: #{print_total_statistics[:total_quantity]}"
-    puts "Requests quantity: #{print_total_statistics[:requests_quantity]}"
+    table = Terminal::Table.new do |t|
+      t.title = 'Statistic'
+      t << ['Total Quantity', print_total_statistics[:total_quantity]]
+      t << ['Requests quantity', print_total_statistics[:requests_quantity]]
+    end
+
+    puts table
   end
 end
