7f0fbb15fa2f1ea6d85b8fa7661b9e5700a012b8
Vitaliy Replyuk <vitaliy.replyuk@student.tuke.sk>
2022-01-10 15:05:06 +0100

    add search.rb

--- lib/menu.rb
+++ lib/menu.rb
@@ -5,17 +5,18 @@ require_relative 'rules'
 require_relative 'result_printer'
 require_relative 'statistics'
 require_relative 'language'
+require_relative 'search'
 require 'i18n'
 
 class Menu
   MENU = { search_a_car: 1, show_all_cars: 2, help: 3, exit: 4 }.freeze
   FILE_CARS = 'cars'
-  FILE_SEARCHES = 'searches'
 
   def initialize
     @database = Database.new
     @cars = @database.read(FILE_CARS)
-    @read_searches = @database.read(FILE_SEARCHES, create: true)
+    @printer = ResultPrinter.new
+    @search = Search.new
   end
 
   def show_menu
@@ -27,8 +28,6 @@ class Menu
       MENU.each do |key, value|
         send(key) if value == option_number
       end
-
-      break if option_number == MENU[:exit]
     end
   end
 
@@ -48,42 +47,7 @@ class Menu
   end
 
   def search_a_car
-    search_by_rules = Rules.new(@cars)
-    statistics = Statistics.new(@read_searches)
-
-    search_by_rules.ask_rules unless search_by_rules.finished?
-
-    match_cars = search_by_rules.match_cars
-
-    statistics.make_total_quantity(match_cars)
-    requests_values = statistics.valuable_request_values(search_by_rules.user_answers)
-    total_statistic = statistics.total_statistic(requests_values)
-
-    @database.write(FILE_SEARCHES, total_statistic)
-
-    sort_option = ask_option(search_by_rules, match_cars)
-    sort_direction = ask_direction(search_by_rules, sort_option)
-
-    print_total_statistics = statistics.find_statistic(requests_values)
-    print_serched_car(print_total_statistics, sort_direction)
-  end
-
-  def ask_option(search_by_rules, match_cars)
-    puts I18n.t('sort_fields.option')
-    puts I18n.t('sort_fields.option_letter')
-    search_by_rules.sort_option(match_cars)
-  end
-
-  def ask_direction(search_by_rules, sort_option)
-    puts I18n.t('sort_fields.direction')
-    puts I18n.t('sort_fields.direction_letter')
-    search_by_rules.sort_direction(sort_option)
-  end
-
-  def print_serched_car(print_total_statistics, sort_direction)
-    printer = ResultPrinter.new
-    printer.print_statics(print_total_statistics)
-    printer.print_result(sort_direction)
+    @search.search_a_car
   end
 
   def show_all_cars
---
+++ lib/search.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require_relative 'database'
+require_relative 'rules'
+require_relative 'result_printer'
+require_relative 'statistics'
+require_relative 'language'
+require 'i18n'
+
+class Search
+  FILE_CARS = 'cars'
+  FILE_SEARCHES = 'searches'
+
+  def initialize
+    @database = Database.new
+    @cars = @database.read(FILE_CARS)
+    @read_searches = @database.read(FILE_SEARCHES, create: true)
+    @printer = ResultPrinter.new
+  end
+
+  def search_a_car
+    search_by_rules = Rules.new(@cars)
+    statistics = Statistics.new(@read_searches)
+    search_by_rules.ask_rules unless search_by_rules.finished?
+    match_cars = search_by_rules.match_cars
+    statistics.make_total_quantity(match_cars)
+    requests_values = statistics.valuable_request_values(search_by_rules.user_answers)
+    total_statistic = statistics.total_statistic(requests_values)
+    @database.write(FILE_SEARCHES, total_statistic)
+    sort_option = ask_option(search_by_rules, match_cars)
+    sort_direction = ask_direction(search_by_rules, sort_option)
+
+    print_total_statistics = statistics.find_statistic(requests_values)
+    print_serched_car(print_total_statistics, sort_direction)
+  end
+
+  def ask_option(search_by_rules, match_cars)
+    puts I18n.t('sort_fields.option')
+    puts I18n.t('sort_fields.option_letter')
+    search_by_rules.sort_option(match_cars)
+  end
+
+  def ask_direction(search_by_rules, sort_option)
+    puts I18n.t('sort_fields.direction')
+    puts I18n.t('sort_fields.direction_letter')
+    search_by_rules.sort_direction(sort_option)
+  end
+
+  def print_serched_car(print_total_statistics, sort_direction)
+    @printer.print_statics(print_total_statistics)
+    @printer.print_result(sort_direction)
+  end
+end
